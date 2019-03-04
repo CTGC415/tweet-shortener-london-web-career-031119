7a104a7e3b4a5ef112ab78dd72dfdb0764e33ede
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-04 19:02:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+require "pry"
+def dictionary
+  hash = {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    for: "4",
+    four: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&"
+  }
+end
+
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  words.map do |word|
+    binding.pry
+    if dictionary.keys.include? word
+      binding.pry
+      word = dictionary[word]
+    end
+  end
+  words.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+
+end
+
+def selective_tweet_shortener(tweet)
+
+end
+
+def shortened_tweet_truncator(tweet)
+
+end
